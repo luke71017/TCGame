cdf769c7994a04b17bf8ab7b4c7864bd523db0aa
unknown <alessio.provenzano@gmail.com>
2016-02-28 12:01:25 +0000

    Prima versione della milestone 1.

---
+++ Assets/Script/logicaPercorso.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logicaPercorso : MonoBehaviour {
+
+	public GameObject pg;
+	private bool grounded = true;
+	private Rigidbody rb;
+	private BoxCollider boxCollider;
+
+	// Use this for initialization
+	void Start () {
+		rb = pg.GetComponent<Rigidbody>();
+		boxCollider = GetComponent<BoxCollider> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit = new RaycastHit();
+
+		//Raycast verifica che il PG sia per terra con soglia a 0.5f e con velocità nulla
+		grounded = Physics.Raycast(pg.transform.position, Vector3.down, out hit, 0.5f) && rb.velocity.y <= 0f;
+
+		if (grounded && hit.collider.Equals(boxCollider)) {
+			print ("fine livello");
+		}
+	}
+}
